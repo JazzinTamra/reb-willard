c54b343509680330ebcf9bd9c8602fdc05c01b95
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-10-25 11:10:47 -0600

    Completed Milestone 1.

--- public-html/documentation/milestone1.php
+++ public-html/documentation/milestone1.php
@@ -1,8 +1,38 @@
 <!DOCTYPE html>
-	<html lang="en">
+<html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Milestone 1: Reb Willard Artist</title>
+		<title>Milestone 1: Website for the artist Reb Willard</title>
 	</head>
+	<body>
+		<main>
+			<h1>Milestone 1: Website for the artist Reb Willard </h1>
+
+			<p><strong>Purpose:</strong> Introduce and showcase the paintings and photographs of Reb Willard in a
+				compelling way to gathers leads, so future sales can be made. Also allows for the easy social media sharing
+				of Reb's art to further market her work. </p>
+
+			<p><strong>Audience:</strong> This site will be for potential customers, businesses that are related to interior design, and people who enjoy art.</p>
+			<p>Individual customers are upper middle class. They own their own home or rent a high-end apartment. Individuals fall into these categories:</p>
+			<ul>
+				<li>Loves interior design. Every space is a place to create something special.</li>
+				<li>Enjoys collecting unique items from crafters, artists, photographers, etc.</li>
+				<li>Enjoys festivals such as gallery strolls, gallery openings, renaissance fairs, Shakespeare festivals
+					etc.</li>
+				<li>Would rather see a play or dance performance than watch TV.</li>
+				<li>Is an artist herself.</li>
+			</ul>
+			<p><strong>Goals:</strong>
+			</p>
+			<ul>
+				<li>To serve as a gallery to showcase and promote Reb's work.</li>
+				<li>To increase sales of paintings and photographs by Reb Willard.</li>
+				<li>To increase the reach and establish more of a social media presence for Reb Willard's work (as measured
+					via analytics tools).
+				</li>
+				<li>To help me showcase my web development skills leading to a full-time great paying job.</li>
+			</ul>
+		</main>
+	</body>
 
 </html>
