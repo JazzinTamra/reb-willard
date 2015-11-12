0a18745fe4053a5f430563c942c1be0104b3d9ec
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-11-12 05:44:37 -0700

    Milestone 2 finished.

--- public_html/documentation/milestone2.php
+++ public_html/documentation/milestone2.php
@@ -70,12 +70,12 @@
 				<li>About Reb</li>
 				<li>Connect</li>
 			</ul>
-			<p><strong>Home Page:</strong> A page filled with a close up of one of Reb’s works. Possibly a slide show of a
+			<p><strong>Home Page:</strong> A page filled with a close up of one of Reb&#146;s works. Possibly a slide show of a
 				few painting and/or a quote from the artist. The navigation bar at the top and social media links in the footer.</p>
 
 			<p><strong>Selected Work:</strong> 8 - 10 examples of Reb&#146;s acrylics and photographs works.</p>
 
-			<p><strong>About Reb:</strong> This page will have a picture of Reb’s with a bio and a paragraph about her as an artist.</p>
+			<p><strong>About Reb:</strong> This page will have a picture of Reb&#146;s with a bio and a paragraph about her as an artist.</p>
 
 			<p><strong>Contact:</strong> This page will contain and interesting image, a quote from Reb and a simple contact form to capture the information on those that are interested in her work.
 			<p><strong>Note:</strong> Every page will have social media connections in the footer.</p>
