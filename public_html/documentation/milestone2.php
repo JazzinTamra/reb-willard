bb20b55309e7940a6fcf62598eec46cb17884a24
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-11-12 05:38:27 -0700

    Milestone 2 Working.

--- public_html/documentation/milestone2.php
+++ public_html/documentation/milestone2.php
@@ -10,49 +10,76 @@
 
 			<h2>Introduction</h2>
 
-			<p>Rebecca Willard's web site will be a destination for people interested in art.</p>
+			<p>Below is a proposed persona and use case for Reb Willard&#146;s web site. This site is meant to serve as
+				a gallery for her works to help increase her sales and establish a professional social media presence.</p>
 
 			<h2>Persona</h2>
 
-			<p><strong>Name:</strong> Nancy Salem </p>
+			<p><strong>Name:</strong> Nancy Reid</p>
 
 			<p><strong>Age:</strong> 55</p>
 
 			<p><strong>Profession:</strong> Upper level manager at Sandia National Labs in change of communication inside
-				and outside the facility. Nancy works hard, but enjoys a rich personal life with her family and friends. She
-				enjoys fine dining, art and fashion. She is well known in the community for her charity, but in general
-				sticks to a few good friends. She has a home in Albuquerque that is beautifully decorated and cabin in Taos
-				that she uses when she goes skiing 5 or 6 times a year.</p>
-
-			<p><strong>Technology:</strong> At work Nancy has access to a PC. She works with the Microsoft Office Suite and
-				other proprietary publishing software. At home Nancy owns a 21.5-inch iMac 1.4GHz dual-core Intel Core i5.
-				She likes it for the styling and the larger screen. She owns a iPhone, but prefers to browse the internet on
+				and outside the facility. Nancy works hard, and enjoys a rich personal life with her family and friends. She
+				enjoys fine dining, art and fashion. She is well known in the community because of her work in the media and her philanthrop. She socializes often with friends. She has a home in Albuquerque that is beautifully decorated and cabin in Taos
+				that she uses when she and her family go skiing 5 or 6 times a year.</p>
+			<p><strong>Technology:</strong> At work Nancy has uses a PC running Windows 8.1. She works with the Microsoft Office Suite and
+				other proprietary publishing software. Her browser is the latest version of Internet Explorer 11. At home Nancy owns a 21.5-inch iMac 1.4GHz dual-core Intel Core i5 and runs Safari for her browser.
+				She likes the iMac for its styling and the larger screen. Nancy owns a iPhone, but prefers to browse the internet on
 				a desktop because of her poor eyesight. Nancy is not especially technical, but has learned the programs
 				needed for her work and hobbies.</p>
 
 			<p><strong>Attitudes and Behaviors: </strong> Nancy loves fashion and art. she enjoys spending time on the
-				internet collecting. Ebay is one of her favorite sites. She likes to watch for luxury brands and only bids
+				internet collecting. eBay is one of her favorite sites. She likes to watch for luxury brands and only bids
 				on the the best items for which she is willing to pay more.</p>
 
-			<p><strong>Frustrations and Needs:</strong> In addition to the time Nancy spends /“collecting/” online, she also
+			<p><strong>Frustrations and Needs:</strong> In addition to the time Nancy spends &#147;collecting&#148; online,
+				she also
 				enjoys browsing for new artist and works of art. There is always room in her collection for a new piece.
-				Nancy finds that many of the gallery and artist websites are not very pretty.</p>
+				Nancy finds that many of the gallery and artist websites are not very appealing.</p>
 
-			<p><strong>Goals:</strong> Nancy goal is to sit down this evening with a nice glass of wine and peruse the web
-				for new works of art. She has nothing specific in mind just 45 minutes of relaxing and browsing.</p>
+			<p><strong>Goals:</strong> Nancy&#146;s goal is to sit down this evening with a nice glass of wine and peruse the web
+				for new works of art. She has nothing specific in mind just 45 minutes of relaxing and browsing before
+				dinner. Our goal is to catch her attention with Reb Willard&#146;s site, so we can capture her contact information for future communication and to increase the chaces of a future sale.</p>
+			<img src="img/site-flow.jpg" alt="Reb's website flow" >
 
 			<h2>Use Case</h2>
 
 			<p>Nancy has the evening free and is planning on opening a nice bottle of wine, relax and peruse the web for
-				new artist that strike her fancy. Nancy is viewing the site on her 21.5-inch iMac at home.
+				new artists that strike her fancy. Nancy is viewing the site on her 21.5-inch iMac at home using Safari.
+			</p>
 			<ol>
-				<li>Nancy lands on Home page. She enjoys the image, so decides to investigates further.</li>
-				<li>Clicking on the Selected Works tab</li>
-				<li>She scrolls through a number of Reb’s works enjoying the acrylics and photographs.</li>
-				<li>From the selected works page Nancy navigates to the About Reb page. She really enjoys the bio and relates to
-				Reb, so moves to the Connect page.</li>
+				<li>Nancy lands on Home page. The site is simple and she enjoys the image of the painting. She decides to
+					investigates further.
+				</li>
+				<li>She clicks on the Selected Works tab.</li>
+				<li>She scrolls through a number of Reb&#146;s works enjoying the acrylics and photographs.</li>
+				<li>From the selected works page Nancy navigates to the About Reb page. She really enjoys the bio and can
+					relate to Reb so moves to the Connect page.
+				</li>
 				<li>Where she fills out the form to be emailed the periodic newsletter.</li>
 			</ol>
+
+			<h2>Proposed Site Map</h2>
+
+			<p>Site will consist of 4 top-level pages total. No sub-pages or sub-navigation. This will be the Minimum
+				Viable Product.</p>
+			<ul>
+				<li>Home Page</li>
+				<li>Selected Works</li>
+				<li>About Reb</li>
+				<li>Connect</li>
+			</ul>
+			<p><strong>Home Page:</strong> A page filled with a close up of one of Reb’s works. Possibly a slide show of a
+				few painting and/or a quote from the artist. The navigation bar at the top and social media links in the footer.</p>
+
+			<p><strong>Selected Work:</strong> 8 - 10 examples of Reb’s acrylics and photographs works.</p>
+
+			<p><strong>About Reb:</strong> This page will have a picture of Reb’s with a bio and a paragraph about her as an artist.</p>
+
+			<p><strong>Contact:</strong> This page will contain and interesting image, a quote from Reb and a simple contact form to capture the information on those that are interested in her work.
+			<p><strong>Note:</strong> Every page will have social media connections in the footer.</p>
+
 		</main>
 	</body>
 
