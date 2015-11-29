3e13e99c3059e4ac30de377b44ca0b2118f34856
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-11-29 15:17:10 -0700

    Milestone 3 finished.

---
+++ public_html/documentation/milestone3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Milestone 3: Website for the artist Reb Willard</title>
+	</head>
+	<body>
+		<main>
+			<h1>Milestone 3: Website for the artist Reb Willard </h1>
+
+			<h2>Wireframes for RebWillard.com</h2>
+			<p>Below are the proposed wireframes for Reb Willard&#146;s web site. I've included wireframes for the four top level pages of her site. If I need to limit the project to two pages, I would like to do the home and selected works pages. This site is meant to serve as a gallery for Reb&#146;s work to help increase her sales and establish a professional social media presence. Reb does not currently have a mailing list, so I will need to set one up from scratch.</p>
+			<img src="img/reb-willard-wireframe.svg" alt="Reb's website wireframe" >
+
+		</main>
+	</body>
+
+</html>
